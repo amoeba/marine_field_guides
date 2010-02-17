7c7d283bcb9299f17e88090875be79ce6a306bac
Bryce Mecum <petridish@gmail.com>
2010-02-17 13:31:48 -0800

    Rails3 controller style

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,46 +1,36 @@
 class SpeciesController < ApplicationController
+  respond_to :html
+  
   def index
-    @species = Species.find(:all)
+    respond_with(@species = Species.all)
   end
   
   def show
-    @species = Species.find(params[:id])
+    respond_with(@species = Species.find(params[:id]))
   end
   
   def new
-    @species = Species.new
+    respond_with(@species = Species.new)
   end
   
   def create
-    @species = Species.new(params[:species])
-    
-    if(@species.save)
-      flash[:notice] = "Successfully created Species"
-      redirect_to(@species)
-    else
-      render(:action => "new")
-    end
+    @species = Species.create(params[:species])
+    respond_with(@species)
   end
   
   def edit
-    @species = Species.find(params[:id])
+    respond_with(@species = Species.find(params[:id]))
   end
   
   def update
     @species = Species.find(params[:id])
-    
-    if(@species.update_attributes(params[:species]))
-      flash[:notice] = "Successfully updated Species"
-      redirect_to(species_instance_url(@species))
-    else
-      render(:action => 'edit')
-    end
+    @species.update_attributes(params[:species])
+    respond_with(@species)
   end
   
   def destroy
     @species = Species.find(params[:id])
     @species.destroy
-    flash[:notice] = "Successfully deleted Species"
-    redirect_to(species_url)
+    redirect_to(:controller => :species)
   end
 end
