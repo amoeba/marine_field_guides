1ba7d71963dfd897d7feb6ffb59950486de3029c
Bryce Mecum <petridish@gmail.com>
2010-02-17 13:35:33 -0800

    Revert app.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
-function insert_fields(link, method, content) {
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+var insert_fields = function(link, method, content) {
   var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + method, "g")
-  $("#"+method).append("<li>"+ content.replace(regexp, new_id) + "</li>");
+  var regexp = new RegExp("new_" + method, "g");
+  $("#"+method).append("<li>" + content.replace(regexp, new_id) + "</li>");
 }
