9efc00546fbc945d13a301b7539b59d5cd890436
Bryce Mecum <petridish@gmail.com>
2010-03-10 13:21:43 -0800

    Hook Nokogiri and open-uri up for ITIS fetching

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module FieldGuides
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password
+    
+    require 'nokogiri'
+    require 'open-uri'
   end
 end
