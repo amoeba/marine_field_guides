82fc978b2c9da748d4e3d60d663dac9e3a131f45
Bryce Mecum <petridish@gmail.com>
2010-03-25 11:33:23 -0700

    Begin expanding the Guide controller

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -1,8 +1,21 @@
 class GuideController < ApplicationController
   def index
+    @species = []
+  end
+  
+  def generate
     @species = Species.find(params[:species_ids].split(","))
-    @guide = ExtendedGuide.new(@species)
+    @style = params[:style]
     
-    send_data(@guide.render, { :filename => "Field Guide.pdf", :type => Mime::PDF})
+    case @style
+    when "simple"
+      @guide = SimpleGuide.new(@species, options)
+    when "gallery"
+      @guide = GalleryGuide.new(@species, options)
+    else
+      @guide = ExtendedGuide.new(@species, options)
+    end
+    
+    send_data(@guide.render, { :filename => "Field Guide.pdf", :type => Mime::PDF}) 
   end
 end
