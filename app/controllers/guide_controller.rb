cb3e7a5039d4eee86f1c8c0896e613774719ca8f
Bryce Mecum <petridish@gmail.com>
2010-04-09 17:13:36 -0700

    Rewrite guide controller

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -1,21 +1,49 @@
 class GuideController < ApplicationController
+  DEFAULTS = {
+      "title" => "Marine Field Guide",
+      "subtitle" => "Custom Selection of Species",
+      "style" => "extended",
+      "color" => "000000",
+    }
+    
   def index
-    @species = []
+    if !params[:species].nil? and params[:species].length > 0
+      @species = Species.find(* params[:species].collect { |s| s["id"] })
+    end
   end
   
   def generate
-    @species = Species.find(params[:species_ids].split(","))
-    @style = params[:style]
-    
-    case @style
-    when "simple"
-      @guide = SimpleGuide.new(@species, options)
-    when "gallery"
-      @guide = GalleryGuide.new(@species, options)
-    else
-      @guide = ExtendedGuide.new(@species, options)
+    if params.nil? or !params.key? "species"
+      flash[:notice] = "You must specify at least one species for your field guide."
+      redirect_to :action => :index
+      return
     end
     
-    send_data(@guide.render, { :filename => "Field Guide.pdf", :type => Mime::PDF}) 
+    @species = Species.find(*params["species"].map { |s| s["id"].to_i })
+    
+    # Fix find's habit of turning a single result into Species instead of an Array
+    @species = [@species] unless @species.is_a?(Array)
+    
+    @options = process_guide_parameters(params["options"])
+    
+    
+    case @options["style"]
+    when "simple"
+      @guide = SimpleGuide.new(@species, @options)
+    when "gallery"
+      @guide = GalleryGuide.new(@species, @options)
+    else
+      @guide = ExtendedGuide.new(@species, @options)
+    end
+    
+    send_data(@guide.render, {
+      :filename => "Field Guide.pdf", 
+      :type => Mime::PDF}
+    ) 
+  end
+  
+  protected
+  def process_guide_parameters(opts = {})
+    DEFAULTS.merge!(opts.reject { |k,v| v.length == 0 })
   end
 end
