0e135c49c632fce6dcb6df5add01839efed00619
Bryce Mecum <petridish@gmail.com>
2010-04-09 17:06:00 -0700

    protected, not private

--- app/controllers/itis_controller.rb
+++ app/controllers/itis_controller.rb
@@ -11,10 +11,9 @@ class ItisController < ApplicationController
     end
     
     respond_with(@taxonomy.to_json, :status => :ok)
-    #render :text => @taxonomy.inspect
   end
   
-  private
+  protected
   def tsn_from_binomial_name(name)
     doc = Nokogiri::XML(open("http://www.cbif.gc.ca/pls/itisca/taxastep?hierarchy=y&king=every&p_action=containing&taxa=#{name.gsub(" ", "+")}&p_format=xml&p_ifx=cbif"))
     tsn = doc.css("tsn")
@@ -29,6 +28,7 @@ class ItisController < ApplicationController
     
     doc.css("parent").each do |taxon|
       rank = taxon.at("rank").content.downcase
+      rank = "klass" if rank == "class"
       name = taxon.at("concatenatedname").content
       @taxonomy[rank.to_sym] = name
     end
