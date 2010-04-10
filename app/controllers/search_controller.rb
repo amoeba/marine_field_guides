6f1b82f4a9687d4345ab97694a8f520729621ff2
Bryce Mecum <petridish@gmail.com>
2010-04-09 17:07:51 -0700

    Rewrite autocompleter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,25 @@
 class SearchController < ApplicationController
+  respond_to :html, :json
+  
   def index
+    if params[:scientific_name].nil? or params[:scientific_name].length == 0
+      flash[:notice] = "You must enter a scientific name"
+    else
+      names = params[:scientific_name].split(" ")
+      
+      query = "genus LIKE ?"
+      values = ["%#{names[0]}%"]
+      
+      if names.length > 1
+        query << " AND species_name LIKE ?"
+        values << "%#{names[1]}%"
+      end
+    
+      @species = Species.where([query, *values]).joins(:taxonomy).includes(:taxonomy)
+    end
+  end
+  
+  def advanced
     if params[:filters]      
       @filters = process_filter_params(params[:filters])
       
@@ -18,25 +38,39 @@ class SearchController < ApplicationController
   end
   
   def autocomplete
-    values = params[:term].split(" ").map { |value| value + "%" }
-    clause = "genus LIKE ?"
-    clause << " AND species_name LIKE ?" if values.length > 1
+    terms = params[:term].split(" ").map { |value| "%#{value}%" }
     
-    @species = Species.joins(:taxonomy).where([clause, *values])
+    clause = ""
+    values = []
     
-    render :json => @species.map { |species| { :label => species.scientific_name, :value => species.id.to_s } }
+    terms.each do |term|
+      clause << "genus LIKE ? OR species_name LIKE ? OR common_names.value LIKE ? "
+      values << term << term << term
+    end
+    
+    @species = Species.joins(:taxonomy, :common_names).includes(:taxonomy, :common_names).where([clause, *values])
+    @results = @species.uniq.map { |species| { 
+      :label => "<span class=\"scientific-name\">" + species.scientific_name + "</span> (" + species.common_name + ")", 
+      :scientific_name => species.scientific_name,
+      :value => species.id.to_s } 
+    }
+    
+    respond_with(@results.to_json, :status => :ok)
   end
   
-  private
+  protected
   def process_filter_params(filter_params = [])
     return nil if filter_params.empty?
     
     @filters = {}
     
     filter_params.each do |filter|
+      # Don't process incomplete filters
+      next if filter.detect { |k,v| v == "" }
+      
       assoc = filter["association"].to_sym
       
-      @filters[assoc] = [] unless @filters[assoc]
+      @filters[assoc] ||= []
       @filters[assoc] << process_filter(assoc, filter)
     end
   
